5d93f8c43ecd888f4a8d8c23aca153de8e621360
Bill-GD <116184478+Bill-GD@users.noreply.github.com>
2024-03-08 15:00:58 +0700

    Safe area for search page

--- lib/search/search_page.dart
+++ lib/search/search_page.dart
@@ -51,76 +51,78 @@ class _SearchPage extends State<SearchPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.background,
-        leading: IconButton(
-          icon: const Icon(Icons.keyboard_arrow_up_rounded, size: 40),
-          onPressed: () async {
-            if (context.mounted) Navigator.of(context).pop();
-          },
-        ),
-        title: Container(
-          height: AppBar().preferredSize.height * 0.65,
-          margin: const EdgeInsets.only(right: 15),
-          child: TextField(
-            autofocus: true,
-            onChanged: searchSongs,
-            decoration: textFieldDecoration(
-              context,
-              hintText: 'Search songs and artists',
-              prefixIcon: Icon(
-                Icons.search_rounded,
-                color: Theme.of(context).colorScheme.primary,
-              ),
-              border: OutlineInputBorder(
-                borderSide: BorderSide(
-                  color: Theme.of(context).colorScheme.onBackground,
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.background,
+          leading: IconButton(
+            icon: const Icon(Icons.keyboard_arrow_up_rounded, size: 40),
+            onPressed: () async {
+              if (context.mounted) Navigator.of(context).pop();
+            },
+          ),
+          title: Container(
+            height: AppBar().preferredSize.height * 0.65,
+            margin: const EdgeInsets.only(right: 15),
+            child: TextField(
+              autofocus: true,
+              onChanged: searchSongs,
+              decoration: textFieldDecoration(
+                context,
+                hintText: 'Search songs and artists',
+                prefixIcon: Icon(
+                  Icons.search_rounded,
+                  color: Theme.of(context).colorScheme.primary,
                 ),
-                borderRadius: BorderRadius.circular(25),
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Theme.of(context).colorScheme.onBackground,
+                  ),
+                  borderRadius: BorderRadius.circular(25),
+                ),
+                fillColor: Theme.of(context).colorScheme.secondaryContainer.withOpacity(0.4),
               ),
-              fillColor: Theme.of(context).colorScheme.secondaryContainer.withOpacity(0.4),
             ),
           ),
         ),
-      ),
-      body: ListView.builder(
-        padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
-        itemCount: filteredSongs.length,
-        itemBuilder: (context, index) {
-          final song = Globals.allSongs.firstWhere((e) => e.absolutePath == filteredSongs[index]);
-          return ListTile(
-            // contentPadding: const EdgeInsets.symmetric(horizontal: 30),
-            title: Text(
-              song.trackName,
-              overflow: TextOverflow.ellipsis,
-              style: const TextStyle(fontWeight: FontWeight.w600),
-            ),
-            subtitle: Text(
-              song.artist,
-              overflow: TextOverflow.ellipsis,
-              style: TextStyle(
-                color: Colors.grey[600],
-                fontWeight: FontWeight.w400,
+        body: ListView.builder(
+          padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+          itemCount: filteredSongs.length,
+          itemBuilder: (context, index) {
+            final song = Globals.allSongs.firstWhere((e) => e.absolutePath == filteredSongs[index]);
+            return ListTile(
+              // contentPadding: const EdgeInsets.symmetric(horizontal: 30),
+              title: Text(
+                song.trackName,
+                overflow: TextOverflow.ellipsis,
+                style: const TextStyle(fontWeight: FontWeight.w600),
               ),
-            ),
-            onTap: () async {
-              FocusManager.instance.primaryFocus?.unfocus();
-              Globals.audioHandler.registerPlaylist(
-                'All songs',
-                Globals.allSongs.map((e) => e.absolutePath).toList(),
-                song.absolutePath,
-              );
-              await Navigator.of(context).push(
-                await getMusicPlayerRoute(
-                  context,
-                  song.absolutePath,
+              subtitle: Text(
+                song.artist,
+                overflow: TextOverflow.ellipsis,
+                style: TextStyle(
+                  color: Colors.grey[600],
+                  fontWeight: FontWeight.w400,
                 ),
-              );
-              setState(() {});
-            },
-          );
-        },
+              ),
+              onTap: () async {
+                FocusManager.instance.primaryFocus?.unfocus();
+                Globals.audioHandler.registerPlaylist(
+                  'All songs',
+                  Globals.allSongs.map((e) => e.absolutePath).toList(),
+                  song.absolutePath,
+                );
+                await Navigator.of(context).push(
+                  await getMusicPlayerRoute(
+                    context,
+                    song.absolutePath,
+                  ),
+                );
+                setState(() {});
+              },
+            );
+          },
+        ),
       ),
     );
   }
