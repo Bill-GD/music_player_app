8f44840b1c5cdd96b21ab85286d5e30f418aa713
Bill-GD <geometrydashbill@gmail.com>
2024-12-16 21:45:47 +0700

    new add album (song) tile
    tile at end of list with plus icon

--- lib/main_screen/songs_list.dart
+++ lib/main_screen/songs_list.dart
@@ -12,6 +12,7 @@ import '../player/music_player.dart';
 class SongList extends StatefulWidget {
   final int param;
   final void Function(void Function()) updateParent;
+
   const SongList({super.key, required this.param, required this.updateParent});
 
   @override
@@ -147,73 +148,70 @@ class _SongListState extends State<SongList> with TickerProviderStateMixin {
                 setState(() {});
               }
             },
-            child: StretchingOverscrollIndicator(
-              axisDirection: AxisDirection.down,
-              child: ListView.builder(
-                itemCount: Globals.allSongs.length,
-                itemBuilder: (context, songIndex) => ListTile(
-                  contentPadding: const EdgeInsets.symmetric(horizontal: 4),
-                  leading: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(left: 12),
-                        child: Icon(
-                          Icons.music_note_rounded,
-                          color: Theme.of(context).colorScheme.primary,
-                        ),
+            child: ListView.builder(
+              itemCount: Globals.allSongs.length,
+              itemBuilder: (context, songIndex) => ListTile(
+                contentPadding: const EdgeInsets.symmetric(horizontal: 4),
+                leading: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 12),
+                      child: Icon(
+                        Icons.music_note_rounded,
+                        color: Theme.of(context).colorScheme.primary,
                       ),
-                    ],
-                  ),
-                  title: Text(
-                    Globals.allSongs[songIndex].name,
-                    overflow: TextOverflow.ellipsis,
-                    style: const TextStyle(fontWeight: FontWeight.w600),
-                  ),
-                  subtitle: Text(
-                    Globals.allSongs[songIndex].artist,
-                    overflow: TextOverflow.ellipsis,
-                    style: TextStyle(
-                      color: Colors.grey[600],
-                      fontWeight: FontWeight.w400,
                     ),
+                  ],
+                ),
+                title: Text(
+                  Globals.allSongs[songIndex].name,
+                  overflow: TextOverflow.ellipsis,
+                  style: const TextStyle(fontWeight: FontWeight.w600),
+                ),
+                subtitle: Text(
+                  Globals.allSongs[songIndex].artist,
+                  overflow: TextOverflow.ellipsis,
+                  style: TextStyle(
+                    color: Colors.grey[600],
+                    fontWeight: FontWeight.w400,
                   ),
-                  onTap: () async {
-                    Globals.audioHandler.registerPlaylist(
-                      'All songs',
-                      Globals.allSongs.map((e) => e.id).toList(),
+                ),
+                onTap: () async {
+                  Globals.audioHandler.registerPlaylist(
+                    'All songs',
+                    Globals.allSongs.map((e) => e.id).toList(),
+                    Globals.allSongs[songIndex].id,
+                  );
+                  await Navigator.of(context).push(
+                    await getMusicPlayerRoute(
+                      context,
                       Globals.allSongs[songIndex].id,
-                    );
-                    await Navigator.of(context).push(
-                      await getMusicPlayerRoute(
-                        context,
-                        Globals.allSongs[songIndex].id,
-                      ),
-                    );
-                    setState(() {});
-                    widget.updateParent(() {});
-                  },
-                  trailing: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Visibility(
-                        visible: Config.currentSortOption == SortOptions.mostPlayed,
-                        child: Text('${Globals.allSongs[songIndex].timeListened}'),
-                      ),
-                      IconButton(
-                        icon: const Icon(Icons.more_vert_rounded),
-                        onPressed: () async {
-                          await showSongOptionsMenu(
-                            context,
-                            Globals.allSongs[songIndex].id,
-                            setState,
-                          );
-                          setState(() {});
-                          widget.updateParent(() {});
-                        },
-                      ),
-                    ],
-                  ),
+                    ),
+                  );
+                  setState(() {});
+                  widget.updateParent(() {});
+                },
+                trailing: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Visibility(
+                      visible: Config.currentSortOption == SortOptions.mostPlayed,
+                      child: Text('${Globals.allSongs[songIndex].timeListened}'),
+                    ),
+                    IconButton(
+                      icon: const Icon(Icons.more_vert_rounded),
+                      onPressed: () async {
+                        await showSongOptionsMenu(
+                          context,
+                          Globals.allSongs[songIndex].id,
+                          setState,
+                        );
+                        setState(() {});
+                        widget.updateParent(() {});
+                      },
+                    ),
+                  ],
                 ),
               ),
             ),
